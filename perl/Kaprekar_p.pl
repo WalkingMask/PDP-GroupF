ab1af7af53224ab9e08fd770801797bc4fddf634
Noritomo Taba <e145709@ie.u-ryukyu.ac.jp>
2016-08-10 07:58:33 +0900

    update_src

--- perl/Kaprekar_p.pl
+++ perl/Kaprekar_p.pl
@@ -5,36 +5,43 @@ use warnings;
 use Parallel::ForkManager;
 use Data::Dumper;
 use Time::HiRes qw(gettimeofday);
+
+#時間計測
 my $start_time = Time::HiRes::time;
-
+#変数宣言
 my ($cnt,$numnum,$num1,$num2,$ans_arr);
-
+#リファレンス生成
 my $ref = [
     [],
     [],
     [],
     []
 ];
-
-for $cnt (0..99) {
+#配列リファレンスを4分割[1,5..][2,6..][3,7..][4,8..]
+for $cnt (0..999999) {
     push @{$ref->[$cnt % 4]}, $cnt; 
 }
 
+#実際に中身はこうなる↓
 #$ref[[0,4..][1,5..][2,6..][3,7..]]
+#取り出す際はでリファレンス_@$ref 
 
-foreach (1..10){
-
+#最大4プロセスでカプレカ数を算出
 my $pm = new Parallel::ForkManager(4);
 
+#10回計測なので単純にforeachで10回回している
+foreach (1..10){
+
 foreach (@$ref){
- my $pid = $pm->start and next;
- 	#print Dumper @$_;
+ my $pid = $pm->start and next; #プロセスをfork
+ 	#print Dumper @$_; Debug : リファレンスの中身確認
 	&Kaprekar(@$_);
  
- $pm->finish;
+ $pm->finish; #子プロセスを終了
 }
 $pm->wait_all_children;
 
+}
 
 sub Kaprekar {
 my (@arr,@buf1,@buf2);
@@ -56,8 +63,5 @@ foreach $cnt (@_){
 }
 
 }
-
-}
-
 
 printf("time = "."%0.3f\n",Time::HiRes::time - $start_time);  
