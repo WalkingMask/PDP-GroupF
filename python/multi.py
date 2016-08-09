aaf9a18614584665fc5a54b342c0a4a5fe049140
monoka <kuria33@gmail.com>
2016-08-10 08:30:41 +0900

    add comments

--- python/multi.py
+++ python/multi.py
@@ -1,28 +1,30 @@
+# -*- coding: utf-8 -*-
 from multiprocessing import Pool
 from multiprocessing import Process
 import time
 
+#カプレカ数を求める関数
 def function(x):
 	START = x
 	a = []
 	i = START
-	digit = 0
+	digit = 0   #桁数
 	if i != 0:
-		while i != 0:
+		while i != 0:  #桁数のカウント
 			i = i / 10
 			digit += 1
 		i = 0
-		while i < digit:
+		while i < digit: #配列に数を詰める
 			x = 10 ** i
 			y = START / x % 10
 			a.append(y)
 			i += 1
-		a.sort()
+		a.sort()         #ソート
 		buf1 = a[:]
-		a.reverse()
+		a.reverse()      #逆ソート
 		buf2 = a[:]
 		i = 0
-		while i < digit:
+		while i < digit: #文字列に変換
 			buf1[i] = str(buf1[i])
 			i += 1
 		i = 0
@@ -30,9 +32,9 @@ def function(x):
 		while i < digit:
 			d += buf1[i]
 			i += 1
-		m1 = int(d)
+		m1 = int(d)      #文字列を数値に変換
 		i = 0
-		while i < digit:
+		while i < digit:　#文字列に変換
 			buf2[i] = str(buf2[i])
 			i += 1
 		i = 0
@@ -40,15 +42,15 @@ def function(x):
 		while i < digit:
 			d += buf2[i]
 			i += 1
-		m2 = int(d)
-		if m2 - m1 == START:
+		m2 = int(d)      #文字列を数値に変換
+		if m2 - m1 == START: #カプレカ数の場合,表示
 			print START
 
-def multi(n):
-	tasks = [[] for i in range(4)]
+def multi(n): #並列化
+	tasks = [[] for i in range(4)] #負荷分散
 	for i in range(n):
 		tasks[i%4].append(i)
-	p = Pool(4)
+	p = Pool(4) #ワークプールを作成
 	for i in range(4):
 		p.map(function, tasks[i])
 
@@ -58,5 +60,5 @@ def main():
 	
 start = time.time()        
 main()
-elapsed_time = time.time() - start
+elapsed_time = time.time() - start #時刻の差から処理時間を計測
 print("elapsed_time:{0}".format(elapsed_time))
--- python/not_multi.py
+++ python/not_multi.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 import time
 start = time.time()
+
+#カプレカ数を求める
 
 START = 1
 END = 1000
@@ -9,22 +12,22 @@ kap_mem = []
 
 while START < END:
 	i = START
-	digit = 0
-	while i != 0:
+	digit = 0    #桁数
+	while i != 0: #桁数のカウント
 		i = i / 10
 		digit += 1
 	i = 0
-	while i < digit:
+	while i < digit: #配列に数を詰める
 		x = 10 ** i
 		y = START / x % 10
 		a.append(y)
 		i += 1
-	a.sort()
+	a.sort()    #ソート
 	buf1 = a[:]
-	a.reverse()
+	a.reverse() #逆ソート
 	buf2 = a[:]
 	i = 0
-	while i < digit:
+	while i < digit: #文字列に変換
 		buf1[i] = str(buf1[i])
 		i += 1
 	i = 0
@@ -32,9 +35,9 @@ while START < END:
 	while i < digit:
 		d += buf1[i]
 		i += 1
-	m1 = int(d)
+	m1 = int(d)  #文字列を数値に変換
 	i = 0
-	while i < digit:
+	while i < digit: #文字列に変換
 		buf2[i] = str(buf2[i])
 		i += 1
 	i = 0
@@ -42,9 +45,9 @@ while START < END:
 	while i < digit:
 		d += buf2[i]
 		i += 1
-	m2 = int(d)
+	m2 = int(d)  #文字列を数値に変換
 	a = []
-	if m2 - m1 == START:
+	if m2 - m1 == START: #カプレカ数の場合,kap_memに追加
 			kap_mem.append(START)
 	START += 1
 
@@ -52,5 +55,5 @@ print '---------------'
 for mem in kap_mem:
 	print mem 
 
-elapsed_time = time.time() - start
+elapsed_time = time.time() - start #時刻の差から処理時間を計測
 print("elapsed_time:{0}".format(elapsed_time))
